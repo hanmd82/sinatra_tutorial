c4e4f66e1bf8bc6d1d6bcf0e0b5a977372557472
Mingding <hanmd82@gmail.com>
2015-04-04 17:34:19 +0800

    First commit from RailsGirls Sinatra tutorial - 'http://guides.railsgirls.com/sinatra-app'

---
+++ sinatra.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'yaml/store'
+
+Choices = {
+  'HAM' => 'Hamburger',
+  'PIZ' => 'Pizza',
+  'CUR' => 'Curry',
+  'NOO' => 'Noodles',
+}
+
+get '/' do
+  @title = 'Welcome to the Sinatra Takeout!'
+
+  erb :index
+end
+
+post '/cast' do
+  @title = 'Thanks for voting!'
+  @vote  = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
+
+  erb :cast
+end
+
+get '/results' do
+  @title = 'Results so far:'
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
+
+  erb :results
+end
